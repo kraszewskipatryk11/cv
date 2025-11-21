31d9c893b0b1bb98f374f3388da250511eddc9dd
unknown <kraszewski.patryk1@gmail.com>
2025-11-21 11:49:02 +0100

    Fix apostrophe in MagicButton title

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
             <a href='mailto:kraszewski-patryk@o2.pl'>
                 <MagicButton
-                title="Let's get in touch"
+                title="Let&apos;s get in touch"
                 icon={<FaLocationArrow />}
                 position='right'
                 />

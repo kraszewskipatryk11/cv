8f2c1449d4c9a684b90437761b19085922656c71
unknown <kraszewski.patryk1@gmail.com>
2025-11-21 11:19:54 +0100

    Added Sentry feedback, updated\ Footer with social links

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,14 +19,14 @@ const RecentProjects = () => {
           {projects.map(({ id, title, des, img, iconLists, link }) => (
             <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[25rem] flex item-center justify-center sm:w-[570px] w-[80vw]'>
               <PinContainer title={link} href={link}>
-                <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
+                <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10 rounded-lg'>
                   <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                     <img src='/bg.png' alt='bg-img' />
                   </div>
                   <img 
                     src={img}
                     alt={title}
-                    className="z-10 absolute bottom-0" />
+                    className="z-10 absolute bottom-0 rounded-lg" />
                 </div>
                 <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
                   {title}
